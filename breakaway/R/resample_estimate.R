0b631731093a099131a3d3e055e7b535bc844eaf
adw96 <adw96@cornell.edu>
2016-10-13 16:23:38 -0500

    Now permits redraws vector of all zeros

--- breakaway/R/resample_estimate.R
+++ breakaway/R/resample_estimate.R
@@ -6,9 +6,13 @@ resample_estimate <- function(my_data, my_function, my_sample_size=NA, ...) {
     warning("Are you sure these are OTU counts?")
   }
   if (is.na(my_sample_size)[1]) my_sample_size <- sum(my_data)
-  my_data <- my_data[my_data > 0]
-  category_names <- paste("sample", 1:length(my_data), sep = "")
-  my_data_long <- rep(category_names, times = my_data)
-  my_resample <- sample(my_data_long, size = sample(my_sample_size), replace = TRUE)
-  my_function(table(my_resample), ...) 
+  my_data2 <- my_data[my_data > 0]
+  if (length(my_data2) > 0) {
+    category_names <- paste("sample", 1:length(my_data2), sep = "")
+    my_data_long <- rep(category_names, times = my_data2)
+    my_resample <- sample(my_data_long, size = sample(my_sample_size), replace = TRUE)
+    my_function(table(my_resample), ...) 
+  } else {
+    my_function(my_data)
+  }
 }
