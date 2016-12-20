ea8d2b9f76f8acd536f0048dad0b3d93dea2ad75
adw96 <adw96@cornell.edu>
2016-12-20 13:11:14 -0500

    remove old lab materials from vignettes

--- vignettes/runcatchall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# A script to run CatchAll on every csv file in (only) the current directory
-
-mylist=$(find . -maxdepth 1 -name "*.csv" -type f) 
-for i in $mylist; do
-  j=${i%.csv};
-  mono ./CatchAllCmdL.exe $i $j 1
-done
