af914a0fb9ce4609c98b431cbddcc5444012d9d2
adw96 <amy.willis7@gmail.com>
2017-11-23 11:58:09 -0800

    Remove unnecessary code duplicates

--- R/richness_breakaway.R
+++ R/richness_breakaway.R
@@ -118,15 +118,21 @@ breakaway <- function(my_data, output=TRUE, plot=FALSE, answers=FALSE) {
   
   ## breakaway's default is to cut off at the first break in frequencies
   
-  ## Horrible way to do this. What was 22 y.o. Amy thinking?!
   ## finds the break in contiguity
-  cutoff <- ifelse(is.na(which(my_data[-1,1]-my_data[-length(my_data[,1]),1]>1)[1]),length(my_data[,1]),which(my_data[-1,1]-my_data[-length(my_data[,1]),1]>1)[1])
+  iss <- my_data[, 1]
+  fis <- my_data[, 2]
+  length_fis <- length(fis)
   
+  breaks <- which(iss[-1] - iss[-length_fis] > 1)
+  cutoff <- ifelse(is.na(breaks[1]), length_fis, breaks[1])
+  
+  ## set up structures
   my_data <- my_data[1:cutoff,]
-  ys <- (my_data[1:(cutoff-1),1]+1)*my_data[2:cutoff,2]/my_data[1:(cutoff-1),2]
+  ratios <- my_data[-1, 2]/my_data[-cutoff, 2]
   xs <- 1:(cutoff-1)
+  ys <- (xs+1)*ratios
   xbar <- mean(xs)
-  lhs <- list("x"=xs-xbar,"y"=my_data[2:cutoff,2]/my_data[1:(cutoff-1),2])
+  lhs <- list("x"=xs-xbar,"y"=ratios)
   
   
   if (cutoff < 6) { ## check for unusual data structures
@@ -150,7 +156,7 @@ breakaway <- function(my_data, output=TRUE, plot=FALSE, answers=FALSE) {
     
     
     weights_inv <- 1/xs
-    run <- minibreak(lhs,xs,ys,my_data,weights_inv)
+    run <- minibreak_all(lhs,xs,ys,my_data,weights_inv, withf1=TRUE)
     result <- list()
     choice <- list()
     
@@ -209,11 +215,11 @@ breakaway <- function(my_data, output=TRUE, plot=FALSE, answers=FALSE) {
           weights1 <- 1/ratiovars
         }
         
-        run <- try ( minibreak(lhs,xs,ys,my_data,1/ratiovars), silent = 1)
+        run <- try ( minibreak_all(lhs,xs,ys,my_data,1/ratiovars, withf1 = TRUE), silent = 1)
         
         if ( class(run) == "try-error") {
           ratiovars <- (p[-1]^2/p[-cutoff]^3 + p[-1]/p[-cutoff]^2)/C
-          run <- try ( minibreak(lhs,xs,ys,my_data,1/ratiovars), silent = 1)
+          run <- try ( minibreak_all(lhs,xs,ys,my_data,1/ratiovars, withf1 = TRUE), silent = 1)
           if ( class(run) == "try-error") {
             if(output) {print("Numerical errors result in non-convergence") }
           }
@@ -236,7 +242,7 @@ breakaway <- function(my_data, output=TRUE, plot=FALSE, answers=FALSE) {
       }
       if( !choice$outcome) {
         if(output) cat("We used 1/x weighting. \n")
-        run <- minibreak(lhs,xs,ys,my_data,weights_inv)
+        run <- minibreak_all(lhs,xs,ys,my_data,weights_inv, withf1 = TRUE)
         choice$outcome <- 1
         choice$model <- rownames(run$useful)[min(which(run$useful[,5]==1))]
         choice$full <-  run[[noquote(choice$model)]]
@@ -326,97 +332,6 @@ richness_clean <- function(count_table, FUN = breakaway) {
                "lower" = sum(count_table[,2]), 
                "upper" = Inf)
   }
-}
-
-minibreak <- function(lhs, xs, ys, my_data, myweights) {
-  structure_1_0 <- function(x,beta0,beta1) (beta0+beta1*x)/(1+x)
-  structure_1_1 <- function(x,beta0,beta1,alpha1) (beta0+beta1*x)/(1+alpha1*x)
-  structure_2_1 <- function(x,beta0,beta1,alpha1,beta2) (beta0+beta1*x+beta2*x^2)/(1+alpha1*x)
-  structure_2_2 <- function(x,beta0,beta1,alpha1,beta2,alpha2) (beta0+beta1*x+beta2*x^2)/(1+alpha1*x+alpha2*x^2)
-  structure_3_2 <- function(x,beta0,beta1,alpha1,beta2,alpha2,beta3) (beta0+beta1*x+beta2*x^2+beta3*x^3)/(1+alpha1*x+alpha2*x^2)
-  structure_3_3 <- function(x,beta0,beta1,alpha1,beta2,alpha2,beta3,alpha3) (beta0+beta1*x+beta2*x^2+beta3*x^3)/(1+alpha1*x+alpha2*x^2+alpha3*x^3)
-  structure_4_3 <- function(x,beta0,beta1,alpha1,beta2,alpha2,beta3,alpha3,beta4) (beta0+beta1*x+beta2*x^2+beta3*x^3+beta4*x^4)/(1+alpha1*x+alpha2*x^2+alpha3*x^3)
-  structure_4_4 <- function(x,beta0,beta1,alpha1,beta2,alpha2,beta3,alpha3,beta4,alpha4) (beta0+beta1*x+beta2*x^2+beta3*x^3+beta4*x^4)/(1+alpha1*x+alpha2*x^2+alpha3*x^3+alpha4*x^4)
-  
-  all_ests <- list()
-  
-  model_1_0 <-  lm(ys~xs,weights=myweights)
-  start_1_0 <- list(beta0 = model_1_0$coef[1], beta1 = model_1_0$coef[2])
-  
-  lhs2 <- lhs; lhs2$x <- xs # otherwise singularity
-  result_1_0 <- try( nls( lhs2$y ~ structure_1_0(x,beta0,beta1), data = lhs2, start_1_0, weights=myweights), silent=T )
-  if(class(result_1_0) == "try-error") {
-    coef_1_0 <- c(start_1_0$beta0,start_1_0$beta1)
-  } else {
-    coef_1_0 <- coef(result_1_0)
-  }
-  
-  beta0_tilde <- coef_1_0[1]
-  beta1_tilde <- coef_1_0[2]
-  xbar <- mean(xs)
-  start_1_1 <- list(beta0 = (beta0_tilde+beta1_tilde*xbar)/(1+xbar), beta1 = beta1_tilde/(1+xbar), alpha1 = 1/(1+xbar))
-  result_1_1 <- try( nls( lhs$y ~ structure_1_1(x,beta0,beta1,alpha1), data = lhs, start_1_1, weights=myweights), silent=T )
-  
-  if(class(result_1_1) == "try-error") {
-    temp_lm <- lm(lhs$y~lhs$x+I((lhs$x)^2),weights=myweights)$coef
-    start_2_1 <- list(beta0 = temp_lm[1], beta1 = temp_lm[2], alpha1 = 0, beta2 = temp_lm[3])
-  } else {
-    start_2_1 <- as.list(c(summary(result_1_1)$coef[,1],"beta2"=0))
-  }
-  result_2_1 <- try(nls( lhs$y ~ structure_2_1(x,beta0,beta1,alpha1,beta2), data = lhs, start_2_1, weights=myweights),silent=T)
-  
-  if(class(result_2_1) == "try-error") {
-    temp_lm <- lm(lhs$y~lhs$x+I((lhs$x)^2),weights=myweights)$coef
-    start_2_2 <- list(beta0 = temp_lm[1], temp_lm[2], alpha1 = 0, beta2 = temp_lm[3], alpha2 = 0)
-  } else start_2_2 <- as.list(c(summary(result_2_1)$coef[,1],"alpha2"=0))
-  result_2_2 <- try(nls( lhs$y ~ structure_2_2(x,beta0,beta1,alpha1,beta2,alpha2), data = lhs, start_2_2, weights=myweights), silent=T)
-  
-  if(class(result_2_2) == "try-error") {
-    temp_lm <- lm(lhs$y~lhs$x+I((lhs$x)^2)+I((lhs$x)^3),weights=myweights)$coef
-    start_3_2 <- list(beta0 = temp_lm[1], beta1 = temp_lm[2], alpha1 = 0, beta2 = temp_lm[3], alpha2 = 0, beta3 = temp_lm[4])
-  } else start_3_2 <- as.list(c(summary(result_2_2)$coef[,1],"beta3"=0))
-  result_3_2 <- try( nls( lhs$y ~ structure_3_2(x,beta0,beta1,alpha1,beta2,alpha2,beta3), data = lhs, start_3_2, weights=myweights), silent=T)
-  
-  if(class(result_3_2) == "try-error") {
-    temp_lm <- lm(lhs$y~lhs$x+I((lhs$x)^2)+I((lhs$x)^3), weights=myweights)$coef
-    start_3_3 <- list(beta0 = temp_lm[1], beta1 = temp_lm[2], alpha1 = 0, beta2 = temp_lm[3], alpha2 = 0, beta3 = temp_lm[4], alpha3 = 0)
-  } else start_3_3 <- as.list(c(summary(result_3_2)$coef[,1],"alpha3"=0))
-  result_3_3 <- try( nls( lhs$y ~ structure_3_3(x,beta0,beta1,alpha1,beta2,alpha2,beta3,alpha3), data = lhs, start_3_3, weights=myweights), silent=T)
-  
-  if(class(result_3_3) == "try-error") {
-    temp_lm <- lm(lhs$y~lhs$x+I((lhs$x)^2)+I((lhs$x)^3)+I((lhs$x)^4),weights=myweights)$coef
-    start_4_3 <- list(beta0 = temp_lm[1], beta1 = temp_lm[2], alpha1 = 0, beta2 = temp_lm[3], alpha2 = 0,beta3 = temp_lm[4], alpha3 = 0,beta4 = temp_lm[5])
-  } else start_4_3 <- as.list(c(summary(result_3_3)$coef[,1],"beta4"=0))
-  result_4_3 <- try( nls( lhs$y ~ structure_4_3(x,beta0,beta1,alpha1,beta2,alpha2,beta3,alpha3,beta4), data = lhs, start_4_3, weights=myweights), silent=T)
-  
-  if(class(result_4_3) == "try-error") {
-    temp_lm <- lm(lhs$y~lhs$x+I((lhs$x)^2)+I((lhs$x)^3)+I((lhs$x)^4),weights=myweights)$coef
-    start_4_4 <- list(beta0 = temp_lm[1], beta1 = temp_lm[2], alpha1 = 0, beta2 = temp_lm[3], alpha2 = 0,
-                      beta3 = temp_lm[4], alpha3 = 0, beta4 = temp_lm[5], alpha4 = 0)
-  } else start_4_4 <- as.list(c(summary(result_4_3)$coef[,1],"alpha4"=0))
-  result_4_4 <- try( nls( lhs$y ~ structure_4_4(x,beta0,beta1,alpha1,beta2,alpha2,beta3,alpha3,beta4,alpha4), data = lhs, start_4_4, weights=myweights), silent=T)
-  
-  info <- list("model_1_0"=result_1_0,"model_1_1"=result_1_1,
-               "model_2_1"=result_2_1,"model_2_2"=result_2_2,"model_3_2"=result_3_2,
-               "model_3_3"=result_3_3,"model_4_3"=result_4_3,"model_4_4"=result_4_4)
-  
-  converged <- lapply(info,class)=="nls"
-  
-  workinginfo <- info[converged]
-  
-  b0est <- lapply(workinginfo,predict,list(x=-xbar))
-  if (length(b0est$model_1_0)!=0) b0est$model_1_0 <- predict(workinginfo$model_1_0,list(x=0))
-  f0est <- lapply(b0est,function(x) my_data[1,2]/x)
-  
-  rootsokay <- as.logical(lapply(workinginfo,rootcheck,lhs,nof1=FALSE))
-  
-  sqerrors <- lapply(workinginfo,sqerror,lhs)
-  residses <- lapply(workinginfo,residse)
-  
-  useable <- rootsokay & (f0est>0)
-  
-  workinginfo$useful <- cbind(f0est,rootsokay,sqerrors,residses,useable)
-  return(workinginfo)
 }
 
 rootcheck <- function(model,lhs,nof1=FALSE) {
@@ -634,7 +549,7 @@ breakaway_nof1 <- function(my_data, output=TRUE, plot=TRUE, answers=FALSE, force
       cat("\tIt looks like you've concatenated some of your my_data!\n Please truncate and try again.\n")
     } else {
       weights_inv <- 1/xs
-      run <- minibreak_nof1(lhs,xs,ys,my_data,weights_inv)
+      run <- minibreak_all(lhs,xs,ys,my_data,weights_inv, withf1 = FALSE)
       result <- list()
       choice <- list()
       
@@ -703,11 +618,11 @@ breakaway_nof1 <- function(my_data, output=TRUE, plot=TRUE, answers=FALSE, force
             weights1 <- 1/ratiovars
           }
           
-          run <- try ( minibreak_nof1(lhs,xs,ys,my_data,1/ratiovars), silent = 1)
+          run <- try ( minibreak_all(lhs, xs, ys, my_data, 1/ratiovars, withf1=FALSE), silent = 1)
           
           if ( class(run) == "try-error") {
             ratiovars <- (p[-1]^2/p[-cutoff]^3 + p[-1]/p[-cutoff]^2)/C
-            run <- try ( minibreak_nof1(lhs,xs,ys,my_data,1/ratiovars), silent = 1)
+            run <- try ( minibreak_all(lhs,xs,ys,my_data,1/ratiovars, withf1=FALSE), silent = 1)
             if ( class(run) == "try-error") {
               if(output) {print("Numerical errors result in non-convergence") }
             }
@@ -730,7 +645,7 @@ breakaway_nof1 <- function(my_data, output=TRUE, plot=TRUE, answers=FALSE, force
         }
         if( !choice$outcome) {
           if(output) cat("Iterative reweighting didn't produce any outcomes after the first iteration, so we use 1/x\n")
-          run <- minibreak_nof1(lhs,xs,ys,my_data,weights_inv)
+          run <- minibreak_all(lhs,xs,ys,my_data,weights_inv, withf1 = FALSE)
           choice$outcome <- 1
           choice$model <- rownames(run$useful)[min(which(run$useful[,5]==1))]
           choice$full <-  run[[noquote(choice$model)]]
@@ -838,7 +753,10 @@ breakaway_nof1 <- function(my_data, output=TRUE, plot=TRUE, answers=FALSE, force
   }
 }
 
-minibreak_nof1 <- function(lhs, xs, ys, my_data, myweights) {
+minibreak_all <- function(lhs, xs, ys, my_data, myweights, withf1 = NULL) {
+  
+  if (is.null(withf1)) stop("Empty argument `withf1`")
+  
   structure_1_0 <- function(x,beta0,beta1) (beta0+beta1*x)/(1+x)
   structure_1_1 <- function(x,beta0,beta1,alpha1) (beta0+beta1*x)/(1+alpha1*x)
   structure_2_1 <- function(x,beta0,beta1,alpha1,beta2) (beta0+beta1*x+beta2*x^2)/(1+alpha1*x)
@@ -863,7 +781,9 @@ minibreak_nof1 <- function(lhs, xs, ys, my_data, myweights) {
   
   beta0_tilde <- coef_1_0[1]
   beta1_tilde <- coef_1_0[2]
-  xbar <- mean(c(1,xs))
+  
+  xbar <- ifelse(withf1, mean(xs), mean(c(1,xs)))
+  
   start_1_1 <- list(beta0 = (beta0_tilde+beta1_tilde*xbar)/(1+xbar), beta1 = beta1_tilde/(1+xbar), alpha1 = 1/(1+xbar))
   result_1_1 <- try( nls( lhs$y ~ structure_1_1(x,beta0,beta1,alpha1), data = lhs, start_1_1, weights=myweights), silent=T )
   
@@ -917,19 +837,33 @@ minibreak_nof1 <- function(lhs, xs, ys, my_data, myweights) {
   b0est <- lapply(workinginfo,predict,list(x=-xbar))
   if (length(b0est$model_1_0)!=0) b0est$model_1_0 <- predict(workinginfo$model_1_0,list(x=0))
   
-  firstratioest <- lapply(workinginfo,predict,list(x=-xbar+1))
-  if (length(firstratioest$model_1_0)!=0) firstratioest$model_1_0 <- predict(workinginfo$model_1_0,list(x=1))
+  if (withf1) {
+    
+    f0est <- lapply(b0est,function(x) my_data[1,2]/x)
+    
+  } else {
+    
+    firstratioest <- lapply(workinginfo,predict,list(x=-xbar+1))
+    if (length(firstratioest$model_1_0)!=0) firstratioest$model_1_0 <- predict(workinginfo$model_1_0,list(x=1))
+    
+    f1est <- lapply(firstratioest,function(x) my_data[1,2]/x)
+    
+    f0est <- as.numeric(f1est)/as.numeric(b0est)
+    
+    
+  }
   
-  f1est <- lapply(firstratioest,function(x) my_data[1,2]/x)
-  
-  f0est <- as.numeric(f1est)/as.numeric(b0est)
-  
-  rootsokay <- as.logical(lapply(workinginfo,rootcheck,lhs,nof1=TRUE))
+  rootsokay <- as.logical(lapply(workinginfo,rootcheck,lhs,nof1=!withf1))
   
   sqerrors <- lapply(workinginfo,sqerror,lhs)
   residses <- lapply(workinginfo,residse)
   
-  useable <- rootsokay & (f0est>0) & (f1est>0)
+  if (withf1) {
+    useable <- rootsokay & (f0est>0)
+  } else {
+    useable <- rootsokay & (f0est>0) & (f1est>0)
+  }
+  
   
   workinginfo$useful <- cbind(f0est,rootsokay,sqerrors,residses,useable)
   return(workinginfo)
