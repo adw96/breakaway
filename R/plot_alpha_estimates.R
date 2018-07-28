60110f919a4a2627765ac41a25d72cff3c4b3252
Bryan Martin <bmartin6@uw.edu>
2018-07-28 15:49:27 -0700

    More robustness. Now passes test-alpha_estimates. Optional plot arguments in progress

--- R/plot_alpha_estimates.R
+++ R/plot_alpha_estimates.R
@@ -1,11 +1,14 @@
 #' Plot function for alpha_estimates class
 #' 
 #' @param x Object of class \code{alpha_estimates}.
-#' @param method Optional. If there are multiple richness methods included in \code{x}, this can be set to the the desired method to be plotted. Defaults to the method of the first alpha diversity estimate.
-#' @param fullCI Optional boolean. Indicates whether you want the plot to include the full confidence intervals. Defaults to \code{FALSE}.
+#' @param measure (Optional). If there are multiple richness measures included in \code{x}, this can be set to the the desired measure to be plotted. Defaults to the measure of the first alpha diversity estimate.
+#' @param color (Optional). Default \code{NULL}. The sample variable to map to different colors. Can be a single character string of the variable name in \code{sample_data} or a custom supplied vector with length equal to the number of samples.
+#' @param shape (Optional). Default \code{NULL}. The sample variable to map to different shapes. Can be a single character string of the variable name in \code{sample_data} or a custom supplied vector with length equal to the number of samples.
+#' @param title (Optional). Default NULL. Character string. The main title for the graphic.
+#' @param trim_plot (Optional). Default \code{FALSE}. Boolean indicator for whether you want the plot to include the full confidence intervals. 
 #' @param ... See details
 #' 
-#' @details ... does not currently have any implemented options. Optional arguments currently include "fullCI", a  Optional
+#' @details ... does not currently have any implemented options. Optional arguments currently include "trim_plot", a  Optional
 #' 
 #' @examples
 #' \dontrun{
@@ -15,11 +18,11 @@
 #' plot(alphas)
 #' }
 #' @export
-plot.alpha_estimates <- function(x, method = NULL, fullCI = FALSE, ...) {
+plot.alpha_estimates <- function(x, measure = NULL, color = NULL, shape = NULL, title = NULL, trim_plot = FALSE, ...) {
   
-  if (is.null(method)) {
-    all_methods <- x %>% lapply(function(x) x$name) %>% unlist %>% unique
-    method <- all_methods[1]
+  if (is.null(measure)) {
+    all_measures <- x %>% lapply(function(x) x$name) %>% unlist %>% unique
+    measure <- all_measures[1]
   }
 
   
@@ -41,15 +44,19 @@ plot.alpha_estimates <- function(x, method = NULL, fullCI = FALSE, ...) {
   if (is.null(uci_tmp)) {
     uci_tmp <- pts_tmp*NA
   }
+  if (length(lci_tmp) != length(pts_tmp) || length(uci_tmp) != length(pts_tmp)) {
+    lci_tmp <- lci_tmp[intersect(names(pts_tmp), names(lci_tmp))]
+    uci_tmp <- uci_tmp[intersect(names(pts_tmp), names(uci_tmp))]
+  }
   
   df <- data.frame(pts = pts_tmp,
-                   lci =  lci_tmp[intersect(names(pts_tmp), names(lci_tmp))],
-                   uci = uci_tmp[intersect(names(pts_tmp), names(uci_tmp))],
+                   lci =  lci_tmp,
+                   uci = uci_tmp,
                    names = names_tmp)
-  yname1 <- method
+  yname1 <- measure
   yname2 <- x[[1]]$estimand
   
-  if (!fullCI) {
+  if (!trim_plot) {
     fiven <- stats::fivenum(df$uci, na.rm = TRUE)
     iqr <- diff(fiven[c(2, 4)])
     if (!is.na(iqr)) {
@@ -73,6 +80,6 @@ plot.alpha_estimates <- function(x, method = NULL, fullCI = FALSE, ...) {
         ggplot2::xlab("") +
         ggplot2::theme_bw() + 
         ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))
-    } # Close last else - happens with no CI or fullCI = TRUE
-  } # Close if (!fullCI)
+    } # Close last else - happens with no CI or trim_plot = TRUE
+  } # Close if (!trim_plot)
 }
