2998ad1f97b33f61db65519533c250e8e7326751
Amy Willis <amy.willis7@gmail.com>
2018-08-01 00:02:10 -0400

    move make_design_matrix to breakaway

---
+++ R/tools_covariate_matrix.R
@@ -0,0 +1,13 @@
+#' Make design matrix
+#' 
+#' @param phyloseq_object A phyloseq object
+#' @param variables variable names
+#' 
+#' @importFrom phyloseq sample_data
+#' @importFrom phyloseq get_variable
+#' 
+#' @export
+make_design_matrix <- function(phyloseq_object, variables) {
+  predictors <- phyloseq_object %>% sample_data %>% get_variable(variables)
+  model.matrix( ~predictors, data = predictors %>% as.data.frame)
+}
