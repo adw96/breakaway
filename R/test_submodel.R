08407b7c3c6f5495fb039b3f4f2ded96fffdc0f1
Pauline Trinh <paulinetrinh@Paulines-MBP.home>
2022-10-04 12:03:06 -0700

    Add in updated .Rbuildignore to ignore non standard files and remove R-CMD-Check notes, add in pasolli_et_al example data to package and remove openxlsx suggested package, update documentation, fix vignette title discrepancy for breakaway.Rmd, update .gitignore to allow for .Rbuildignore update

--- R/test_submodel.R
+++ R/test_submodel.R
@@ -49,7 +49,7 @@
 #' L <- rbind(c(0,1,0),
 #'            c(0,0,1))
 #'
-#' F_test_results <- F_test(example_fit, L)
+#' F_test_results <- F_test(example_fit, L, nboot = 100)
 #'
 #' @export
 test_submodel <- function(fitted_betta,
